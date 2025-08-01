83cd46fd440cf0a1dd2ecb78d91dd38c9f5676ff
Miguel <945507756m@gmail.com>
2025-08-01 14:21:03 -0300

    add tela validacao

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import React, { useState } from 'react';
-// import MenuInicial from './components/MenuInicial';
-// import Jogar from './components/Jogar';
-// import Cadastrar from './components/Cadastrar';
-// import Atualizar from './components/AtualizarPar';
-// import Listar from './components/ListaPares';
-// import Excluir from './components/DeletarPar';
-
-// function App() {
-//   const [telaAtual, setTelaAtual] = useState('menu');
-
-//   const navegar = (destino) => setTelaAtual(destino);
-
-//   return (
-//     <div style={{ padding: '20px' }}>
-//       {telaAtual === 'menu' && <MenuInicial navegar={navegar} />}
-//       {telaAtual === 'jogar' && <Jogar voltar={() => setTelaAtual('menu')} />}
-//       {telaAtual === 'cadastrar' && <Cadastrar voltar={() => setTelaAtual('menu')} />}
-//       {telaAtual === 'atualizar' && <Atualizar voltar={() => setTelaAtual('menu')} />}
-//       {telaAtual === 'listar' && <Listar voltar={() => setTelaAtual('menu')} />}
-//       {telaAtual === 'excluir' && <Excluir voltar={() => setTelaAtual('menu')} />}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react';
 import MenuInicial from './components/MenuInicial'
 import Listar from './components/ListaPares';
